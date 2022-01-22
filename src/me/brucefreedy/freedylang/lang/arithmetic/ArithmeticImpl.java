49c8d194130b74f606e407eabf35d11252076685
Bruce0203 <BruceFreedy@gmail.com>
2022-01-22 11:55:14 +0900

    commit

--- src/me/brucefreedy/freedylang/lang/arithmetic/ArithmeticImpl.java
+++ src/me/brucefreedy/freedylang/lang/arithmetic/ArithmeticImpl.java
@@ -21,7 +21,7 @@ public abstract class ArithmeticImpl extends Number implements Stealer<Object>, 
     @Override
     public void setProcess(Process<?> process) {
         a = process;
-        disabled = false;
+        if (a != this) disabled = false;
     }
 
     @Override
