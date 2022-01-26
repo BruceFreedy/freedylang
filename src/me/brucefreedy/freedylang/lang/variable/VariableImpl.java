9099e6df7934b1c524ea1b9559c604768a833c81
Bruce0203 <BruceFreedy@gmail.com>
2022-01-26 18:04:32 +0900

    commit

--- src/me/brucefreedy/freedylang/lang/variable/VariableImpl.java
+++ src/me/brucefreedy/freedylang/lang/variable/VariableImpl.java
@@ -198,8 +198,7 @@ public class VariableImpl extends ProcessImpl<Object> implements Variable<Object
     @Override
     public Object run(ProcessUnit processUnit, List<?> params) {
         if (this.body != null && this.params == null && assignment == null) {
-            System.out.println("debug!!!");
-            return this;
+            return get();
         }
         VariableRegister scope = processUnit.getVariableRegister();
         if (args.size() <= params.size()) {
