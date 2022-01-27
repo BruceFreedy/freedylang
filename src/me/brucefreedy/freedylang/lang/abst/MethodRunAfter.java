dec2ed973542d2491966c47fefabe0ae8b9a74b5
Bruce0203 <BruceFreedy@gmail.com>
2022-01-27 13:31:10 +0900

    commit

--- src/me/brucefreedy/freedylang/lang/abst/MethodRunAfter.java
+++ src/me/brucefreedy/freedylang/lang/abst/MethodRunAfter.java
@@ -2,5 +2,7 @@ package me.brucefreedy.freedylang.lang.abst;
 
 /**
  * do not run params for custom method
+ * do not Supplier#get process while perform params
+ * do not new instnace of VariableRegister
  */
 public interface MethodRunAfter extends Method { }
